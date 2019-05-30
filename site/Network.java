2926978d8f46639b2290cc7f32d599f7024320b3
Mark Scrimshire <mscrimshire+github@gmail.com>
2019-05-30 08:56:14 -0400

    revert to FHIR R4 Publisher output

--- site/Network.java
+++
@@ -1,7 +0,0 @@
-package https://api-v8-r4.hspconsortium.org/DrugFormulary1/open/ImplementationGuide/usdrugformulary-0.2.0;
-
-import org.hl7.fhir.r5.model.ProfilingWrapper;
-
-public class Network {
-
-}
